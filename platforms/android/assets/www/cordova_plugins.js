10e257704756a3e25edb02331bba3f41a18e7257
Philippe Simpson <philippesimpson@gmail.com>
2015-10-30 11:57:32 +0100

    clean up

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,46 +4,12 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "id": "cordova-plugin-device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification.js",
-        "id": "cordova-plugin-local-notifications-mm.LocalNotification",
-        "clobbers": [
-            "cordova.plugins.notification.local",
-            "plugin.notification.local"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification-core.js",
-        "id": "cordova-plugin-local-notifications-mm.LocalNotification.Core",
-        "clobbers": [
-            "cordova.plugins.notification.local.core",
-            "plugin.notification.local.core"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification-util.js",
-        "id": "cordova-plugin-local-notifications-mm.LocalNotification.Util",
-        "merges": [
-            "cordova.plugins.notification.local.core",
-            "plugin.notification.local.core"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-device": "1.0.1",
-    "cordova-plugin-local-notifications-mm": "0.8.2-dev",
-    "cordova-plugin-android-support-v4": "21.0.1"
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
