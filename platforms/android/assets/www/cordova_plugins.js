6d4a50a745bd91f0676846059733e6384d53ed1a
Philippe Simpson <philippesimpson@gmail.com>
2015-10-30 11:26:31 +0100

    android support v4 plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -11,13 +11,39 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification.js",
+        "id": "cordova-plugin-local-notifications-mm.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification-core.js",
+        "id": "cordova-plugin-local-notifications-mm.LocalNotification.Core",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-local-notifications-mm/www/local-notification-util.js",
+        "id": "cordova-plugin-local-notifications-mm.LocalNotification.Util",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.0.0",
-    "cordova-plugin-device": "1.0.1"
+    "cordova-plugin-device": "1.0.1",
+    "cordova-plugin-local-notifications-mm": "0.8.2-dev",
+    "cordova-plugin-android-support-v4": "21.0.1"
 }
 // BOTTOM OF METADATA
 });
